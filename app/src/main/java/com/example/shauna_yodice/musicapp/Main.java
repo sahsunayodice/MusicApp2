75dd29de719f4aae0fca8e8d3e16d9134547aa3d
sahsunayodice <shauna.yodice@gmail.comm>
2019-03-04 09:59:38 -0500

    button

--- app/src/main/java/com/example/shauna_yodice/musicapp/Main.java
+++ app/src/main/java/com/example/shauna_yodice/musicapp/Main.java
@@ -1,13 +1,31 @@
 package com.example.shauna_yodice.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
+    private Button ClarinetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ClarinetButton= (Button) findViewById(R.id.ClarinetButton);
+        ClarinetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openClarinet();
+            }
+        });
+
     }
+
+    public void openClarinet(){
+        Intent intent= new Intent()
+    }
+
 }
