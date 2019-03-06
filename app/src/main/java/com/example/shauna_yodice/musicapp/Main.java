a558898b92a7b65eb43504f8bdbd5d478a4f2ce8
sahsunayodice <shauna.yodice@gmail.comm>
2019-03-06 10:05:33 -0500

    update

--- app/src/main/java/com/example/shauna_yodice/musicapp/Main.java
+++ app/src/main/java/com/example/shauna_yodice/musicapp/Main.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 
 public class Main extends AppCompatActivity {
     private Button ClarinetButton;
+    private Button AltoButton;
+    private Button FluteButton;
+    private Button FrenchButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +26,60 @@ public class Main extends AppCompatActivity {
             }
         });
 
+        AltoButton=(Button) findViewById(R.id.AltoButton);
+        AltoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlto();
+            }
+        });
+
+        FluteButton=(Button) findViewById(R.id.FluteButton);
+        FluteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFlute();
+            }
+        });
+
+        FrenchButton=(Button) findViewById(R.id.FrenchButton);
+        FrenchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFrench();
+            }
+        });
+
     }
 
     public void openClarinet(){
-        Intent intent= new Intent()
+        Intent intent= new Intent(this, Clarinet.class);
+        startActivity(intent);
     }
+
+    public void openAlto(){
+        Intent intent= new Intent(this,Saxophone.class);
+        startActivity(intent);
+        }
+
+    public void openFlute(){
+        Intent intent= new Intent(this, Flute.class);
+        startActivity(intent);
+    }
+
+    public void openFrench(){
+        Intent intent= new Intent(this, FrenchButon.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
